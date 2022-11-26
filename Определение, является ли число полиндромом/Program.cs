53cd96f5086abb558e5d0a426eb59d90dc60c143
SergeyK <serega_konchakov@mail.ru>
2022-11-26 12:19:09 +0300

    Определение,является ли 5-значное число полиндромом

---
+++ Определение, является ли число полиндромом/Program.cs
@@ -0,0 +1,23 @@
+﻿// Программа определяет, является ли число полиндромом
+Console.WriteLine("Введите пятизначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+if (number/10000 > 0)
+{
+    int i = number/10000;
+    int N = number%10;
+    if (i == N)
+    {
+        i = number/1000;
+        N = number%100;
+        int i1 = i%10;
+        int N1 = N/10;
+        if(i1 == N1)
+        {
+           Console.WriteLine("Число является полиндромом"); 
+        }
+        else Console.WriteLine("Число не является полиндромом во 2 и 4 значении чисел");
+    }
+    else {Console.WriteLine("Число не является полиндромом");}
+}
+else {Console.WriteLine("Введите правильное число: положительное, не начинается с 0");}
+

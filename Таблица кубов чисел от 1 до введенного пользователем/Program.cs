4e3ecfd64e209cf43fd171832f7fa60eb537a08d
SergeyK <serega_konchakov@mail.ru>
2022-11-26 12:30:19 +0300

    Вывод кубов чисел от 1 до введенного пользователем числа

---
+++ Таблица кубов чисел от 1 до введенного пользователем/Program.cs
@@ -0,0 +1,14 @@
+﻿// See https://aka.ms/new-console-template for more information
+void Zadacha23()
+{
+Console.WriteLine ("Введите число: ");
+int N = Convert.ToInt32(Console.ReadLine());
+int number = 1;
+while (number <= N)
+    {
+    Console.WriteLine (Math.Pow(number, 3));
+    number++;
+    }
+}
+Zadacha23();
+
